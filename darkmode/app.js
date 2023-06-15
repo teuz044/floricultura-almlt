4fa3c06d20ec6fd4fbfadd9543b2cf88c6c0d0cf
Luana Paes <114548504+luanapaes@users.noreply.github.com>
2023-06-15 10:34:42 -0300

    changes

---
+++ darkmode/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Ativar modo escuro";
+    } else {
+        this.textContent = "Ativar modo claro";
+    }
+
+    console.log('current class name: ' + className);
+});
